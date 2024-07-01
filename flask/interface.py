9b295068a4193ea9553baaa4cc6940d363462fb5
xiajia <xj1101295740@163.com>
2024-07-01 20:00:39 +0800

    hotfix:修改返回json格式

--- flask/interface.py
+++ flask/interface.py
@@ -149,7 +149,7 @@ def get_data():
 
     return jsonify({"status_code": "200",
                     "message": "操作成功",
-                    "response_data": {"skin:'2a'": {"box_number": len(pred_classes),
+                    "response_data": {"skin:2a": {"box_number": len(pred_classes),
                                                     'score_box': predBoxesNew,
                                                     'pred_classes': pred_classes,
                                                     'color': color,
@@ -157,12 +157,12 @@ def get_data():
                                                         "image_recognition_url": saveUrlPrefix + "RecognitionImg.jpg",
                                                                   }
                                                     },
-                                      "skin:'8a'": {
+                                      "skin:8a": {
                                           "label_imag": {
                                               "image_sensitive_url": saveUrlPrefix + "sensitiveSkinImg.jpg"
                                                         }
                                                    },
-                                      "skin:'11a'": {
+                                      "skin:11a": {
                                           "label_imag": {
                                               "image_contour_url": saveUrlPrefix + "ContourImg.jpg"
                                                         }
